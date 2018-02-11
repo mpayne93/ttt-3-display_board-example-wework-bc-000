daaad629125997d872883af6e6552334b3dcfced
Maurice Payne <maurice.payne@wework.com>
2018-02-11 19:31:08 +0000

    automatically backed up by learn

---
+++ display_board.rb
@@ -0,0 +1,11 @@
+def display_board
+  puts "Welcome to Tic Tac Toe"
+puts "   " "|" "   " "|" "   "
+puts "-----------------"
+puts"   ", "   ","   "
+puts "-----------------"
+puts "   ", "   ","   "
+puts "What move would X like to make  (1-9)?"
+end
+
+display_board
